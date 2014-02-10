cb44f57ce9a4be28263d32153f857c1df2237813
Taco Jan Osinga <info@osingasoftware.nl>
2014-02-10 15:35:23 +0100

    Updates the modal structure to use the global system for using modals.

--- lib/mojura/webapp/views/files/view_main.rb
+++ lib/mojura/webapp/views/files/view_main.rb
@@ -9,7 +9,7 @@ module MojuraWebApp
 		def initialize(options = {})
       @folderid = options[:folderid].to_s
       @folderid = WebApp.page.request_params[:folderid].to_s if (@folderid == '')
-      @folderid = options[:root_folderid].to_s if (@folderid == '')
+      @folderid = options[:base_folderid].to_s if (@folderid == '')
 			@folderid = 'root' if (@folderid == '')
 			params = {}
 			params[:folderid] = @folderid
@@ -23,9 +23,9 @@ module MojuraWebApp
       data[:hide_breadcrumbs] = options[:hide_breadcrumbs].to_s == 'true'
       data[:hide_icons] = options[:hide_icons].to_s == 'true'
       data[:hide_extensions] = options[:hide_extensions].to_s == 'true'
-      data[:root_folderid] = options[:root_folderid].to_s
+      data[:base_folderid] = options[:base_folder].to_s
       data[:has_description] = (!data[:description][:html].empty?) rescue false
-      data[:is_base_folder] = (@folderid == 'root') || (@folderid == data[:root_folderid])
+      data[:is_base_folder] = (@folderid == 'root') || (@folderid == data[:base_folderid])
 			super(options, data)
 			data[:files] ||= []
 			@data[:files].map! { |item|
@@ -37,22 +37,18 @@ module MojuraWebApp
 				item[:is_archive] = (item[:mime_type] == 'application/zip') if !item[:mime_type].nil?
 				item
 			}
-			@modals = [{id: 'modalAddFile', btn_class: 'btn-primary', btn_title: WebApp.locale_str('system', 'action_add'), title: WebApp.locale_str('files', 'action_add_file'), icon: 'fa-plus'},
-			           {id: 'modalEditFile', btn_class: 'btn-primary', btn_title: WebApp.locale_str('system', 'action_edit'), title: WebApp.locale_str('files', 'action_edit_file'), icon: 'fa-pencil'},
-			           {id: 'modalExtractFile', btn_class: 'btn-primary', btn_title: WebApp.locale_str('files', 'action_extract'), title: WebApp.locale_str('files', 'action_extract_file'), icon: 'fa-archive'},
-			           {id: 'modalDeleteFile', btn_class: 'btn-danger', btn_title: WebApp.locale_str('system', 'action_delete'), title: WebApp.locale_str('files', 'action_delete_file'), icon: 'fa-trash-o'},
-			           {id: 'modalAddFolder', btn_class: 'btn-primary', btn_title: WebApp.locale_str('system', 'action_add'), title: WebApp.locale_str('files', 'action_add_folder'), icon: 'fa-plus'},
-			           {id: 'modalEditFolder', btn_class: 'btn-primary', btn_title: WebApp.locale_str('system', 'action_edit'), title: WebApp.locale_str('files', 'action_edit_folder'), icon: 'fa-pencil'},
-			           {id: 'modalDeleteFolder', btn_class: 'btn-danger', btn_title: WebApp.locale_str('system', 'action_delete'), title: WebApp.locale_str('files', 'action_delete_folder'), icon: 'fa-trash-o'}]
-
 			WebApp.page.include_template_file('template_files_folders_container', 'webapp/views/files/view_files_folders.mustache')
-			WebApp.page.include_template_file('template-add-edit-file', File.dirname(__FILE__) + '/view_file_add_edit.mustache')
-			WebApp.page.include_template_file('template-add-edit-folder', File.dirname(__FILE__) + '/view_folder_add_edit.mustache')
-			WebApp.page.include_template_file('template-extract-file', File.dirname(__FILE__) + '/view_file_extract.mustache')
-			WebApp.page.include_template_file('template-delete-file', File.dirname(__FILE__) + '/view_file_delete.mustache')
-			WebApp.page.include_template_file('template-delete-folder', File.dirname(__FILE__) + '/view_folder_delete.mustache')
+      if (WebApp.current_user.logged_in?)
+				WebApp.page.include_template_file('template-files-add-edit-file', 'webapp/views/files/view_file_add_edit.mustache')
+				WebApp.page.include_template_file('template-files-add-edit-folder', 'webapp/views/files/view_folder_add_edit.mustache')
+				WebApp.page.include_template_file('template-files-extract-file', 'webapp/views/files/view_file_extract.mustache')
+				WebApp.page.include_template_file('template-files-delete-file', 'webapp/views/files/view_file_delete.mustache')
+				WebApp.page.include_template_file('template-files-delete-folder', 'webapp/views/files/view_folder_delete.mustache')
+				WebApp.page.include_locale(:system)
+				WebApp.page.include_locale(:files)
+      end
 
-			WebApp.page.include_script("FilesView.setCurrentFolderId('#{folderid}', true);") if (@folderid != 'root')
+			WebApp.page.include_script("FilesView.setCurrentFolderId('#{folderid}', false);") if (@folderid != 'root')
 		end
 
 	end
