71dcde0d27e67fd74ef7fd20c04db507b8315d67
Taco Jan Osinga <info@osingasoftware.nl>
2014-02-14 01:02:29 +0100

    Removes fixed widths of images via the ImageView.

--- lib/mojura/webapp/views/images/view_main.rb
+++ lib/mojura/webapp/views/images/view_main.rb
@@ -6,17 +6,16 @@ module MojuraWebApp
 
 		def render
 			size = @options[:parent_col_span] || 1
-			width = (70 * size) + (30 * (size - 1))
 			ubb = ''
 			@options[:fileids] ||= ''
 			@options[:slideshow] ||= false
 			images = @options[:fileids].split(',')
 			if (images.count > 1) && (@options[:slideshow])
-				ubb = "[slideshow width=#{width}]"
-				images.each { |id| ubb += "__api__/files/#{id}/download?size=#{width}&type=width\n" }
+				ubb = "[slideshow]"
+				images.each { |id| ubb += "__api__/files/#{id}/download\n" }
 				ubb += '[/slideshow]'
 			elsif images.count > 0
-				images.each { |id| ubb += "[img]__api__/files/#{id}/download?size=#{width}&type=width[/img]" }
+				images.each { |id| ubb += "[img]__api__/files/#{id}/download[/img]" }
 			else
 				ubb = 'No image id specified ' + images.to_s
 			end
