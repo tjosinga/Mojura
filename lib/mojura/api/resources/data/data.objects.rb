86f45e306c02182078bea96fedf12776dcca3575
Taco Jan Osinga <info@osingasoftware.nl>
2014-02-24 00:06:41 +0100

    Add API resources Data, which can be used for simple user data. It requires a name and email address of the user, and may contain a text and/or a hash of key values. Very easy for simple forms handling.

---
+++ lib/mojura/api/resources/data/data.objects.rb
@@ -0,0 +1,72 @@
+require 'digest'
+require 'mail'
+require 'api/lib/dbobjects'
+require 'api/resources/groups/groups.objects'
+
+module MojuraAPI
+
+	class DataBlock < DbObject
+
+		def initialize(id = nil)
+			super('data', id)
+		end
+
+		def load_fields
+			yield :type, String, :required => true
+			yield :name, String, :required => true
+			yield :email, String, :required => true, :validations => {is_email: true}
+			yield :timestamp, Time, :required => true, :default => Time.new
+			yield :text, RichText, :required => false
+			yield :values, Hash, :required => false, :default => {}
+		end
+
+		def save_to_db(send_email = true)
+			result = super()
+			send_as_email if send_email
+			return result
+		end
+
+		def send_as_email
+			receiver = Settings.get_s('receiver_' + type, :data)
+			receiver = Settings.get_s(:receiver, :data, 'taco@osisoft.nl') if receiver.empty?
+			API.log.warn("There's no known receiver for data block emails. The data block is stored, but not send as e-mail")
+
+			website = Thread.current[:mojura][:env]['HTTP_HOST']
+
+			subject = Settings.get_s('subject_' + type, :data)
+			subject = Settings.get_s(:subject, :data, sprintf(Locale.str(:data, :default_subject), website)) if subject.empty?
+
+			body = Settings.get_s('body_' + type, :data)
+			body = Settings.get_s(:body, :data, sprintf(Locale.str(:data, :default_body), website)) if body.empty?
+			body = (body + "\n\n").gsub("^\n*", '')
+			body += (text + "\n\n").gsub("^\n*", '')
+			values.each { | k, v |
+				body += "#{k}: #{v}\n"
+			}
+
+			mail = Mail.new
+			mail[:from] = "#{name} <#{email}>"
+			mail[:to] = receiver
+			mail[:subject] = subject
+			mail[:body] = body
+			mail.delivery_method(:sendmail)
+			mail.deliver!
+		end
+
+	end
+
+
+	class DataBlocks < DbObjects
+
+		def initialize(where = {}, options = {})
+			options[:sort] ||= {timestamp: -1}
+			super('data', DataBlock, where, options)
+		end
+
+	end
+
+
+end
+
+
+
---
+++ lib/mojura/api/resources/data/data.rest.rb
@@ -0,0 +1,89 @@
+require 'api/lib/restresource'
+require 'api/lib/restresource_tags'
+require 'api/resources/data/data.objects'
+
+module MojuraAPI
+
+	class DataResource < RestResource
+
+		def name
+			'Data blocks'
+		end
+
+		def description
+			'Resource of data blocks, a basic block of user data, which can be used for all kind of user forms.'
+		end
+
+		def all(params)
+			params[:pagesize] ||= 10
+			result = paginate(params) { |options| DataBlocks.new(self.filter(params), options) }
+			return result
+		end
+
+		def put(params)
+			DataBlock.new.load_from_hash(params).save_to_db.to_a
+		end
+
+		def get(params)
+			DataBlock.new(params[:ids][0]).to_a
+		end
+
+		def post(params)
+			datablock = DataBlock.new(params[:ids][0])
+			datablock.load_from_hash(params)
+			return datablock.save_to_db.to_a
+		end
+
+		def delete(params)
+			datablock = DataBlock.new(params[:ids][0])
+			datablock.delete_from_db
+			return [:success => true]
+		end
+
+		def all_conditions
+			result = {
+				description: 'Returns a list of data blocks. Use pagination and filtering to make selections.',
+			}
+			return result
+		end
+
+		def put_conditions
+			result = {
+				description: 'Creates a data block and returns the object.',
+				attributes: {
+					type: {required: true, type: String, description: 'The type of the data block, a unique identifier for the form.'},
+					name: {required: true, type: String, description: 'The name of the user.'},
+					email: {required: true, type: String, description: 'The email address of the user.'},
+					text: {required: false, type: RichText, description: 'The content of the block in the format. The client is responsible for validating the text.'},
+					values: {required: false, type: String, description: 'A hash of keys and their values. The client is responsible for validating the key-values.'},
+				}
+			}
+			return result
+		end
+
+		def get_conditions
+			{
+				description: 'Returns a data block with the specified dataid',
+			}
+		end
+
+		def post_conditions
+			result =
+				{
+					description: 'Updates a data block with the given keys.',
+					attributes: self.put_conditions[:attributes].each { |_, v| v[:required] = false }
+				}
+			return result
+		end
+
+		def delete_conditions
+			{
+				description: 'Deletes the data blocks'
+			}
+		end
+
+	end
+
+	API.register_resource(DataResource.new('data', '', '[dataid]'))
+
+end
