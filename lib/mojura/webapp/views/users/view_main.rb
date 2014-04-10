4ac2d4c4e1e737140d5dbe5f7074004ae722665f
Taco Jan Osinga <info@osingasoftware.nl>
2014-04-10 17:23:07 +0200

    First part of refactoring of UsersView, which also depreactes the ProfileView.

--- lib/mojura/webapp/views/users/view_main.rb
+++ lib/mojura/webapp/views/users/view_main.rb
@@ -1,46 +1,39 @@
+require 'webapp/mojura/lib/settings'
+
 module MojuraWebApp
 
 	class UsersView < BaseView
 
 		def initialize(options = {})
-			options[:view] ||= 'thumbs'
-			options[:col_span] ||= 2
+			params = WebApp.page.request_params
+			data = {}
+			data[:users_page_url] = Settings.get_s(:default_page_users, :core, 'users')
+			data[:show_extensive] = !params[:userid].to_s.empty?
+			data[:show_avatars] = !data[:show_extensive]
+			data[:show_email] = WebApp.current_user.logged_in? &&
+													((WebApp.current_user.id == params[:userid]) || Settings.get_b(:show_email, :users, false))
+ 			command = (data[:show_extensive]) ? "users/#{params[:userid]}" : 'users'
+      data.merge!(WebApp.api_call(command))
 
-			api_call = (options[:api_call] || 'users')
-			api_options = {}
-			data = WebApp.api_call(api_call, api_options)
+			if (data[:show_extensive] && data[:rights][:update])
+				WebApp.page.include_template_file('template-edit-userinfo', 'webapp/views/users/view_edit_userinfo.mustache')
+				WebApp.page.include_template_file('template-edit-avatar', 'webapp/views/users/view_edit_avatar.mustache')
+				WebApp.page.include_template_file('template-edit-password', 'webapp/views/users/view_edit_password.mustache')
+				WebApp.page.include_template_file('template-edit-groups', 'webapp/views/users/view_edit_groups.mustache')
+				if data[:rights][:delete]
+					WebApp.page.include_template_file('template-deactivate-user', 'webapp/views/users/view_deactivate_user.mustache')
+				end
+				WebApp.page.include_script_link('ext/crypto-js/md5.min.js')
+				WebApp.page.include_locale(:system)
+				WebApp.page.include_locale(:users)
+			end
 
-			options[:parent_col_span] ||= 12
-			options[:col_span] ||= options[:parent_col_span]
-			options[:col_devices] ||= 'md'
-			options[:col_rec] = ((12 / options[:parent_col_span]) * options[:col_span]).floor
-
-			data[:span] = "col-#{options[:col_devices]}-#{options[:col_rec]}"
-			data[:size] = 170
-#			data[:size] = ((options[:col_span] * 70) + ((options[:col_span] - 1) * 30)).to_i
-
-			profile_url = (options[:profile_url] || 'profile/?userid=[userid]')
-			data[:items].each { |item|
-				item[:web_url] = profile_url.gsub(/\[userid\]/, item[:id])
-				item[:thumb_name] = (options[:col_span] == 1) ? item[:firstname] : item[:fullname]
-			}
 			super(options, data)
-		end
-
-		def render
-			super
-		end
-
-		def is_list_view
-			@options[:view] == 'list'
-		end
-
-		def is_thumbs_view
-			@options[:view] == 'thumbs'
 		end
 
 		WebApp.register_view('users', UsersView)
 
 	end
+
 
 end
