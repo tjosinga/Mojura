12a0fe59738162540fad3580b9bbce310839d8ba
Taco Jan Osinga <info@osingasoftware.nl>
2014-04-10 17:19:18 +0200

    Update Modals.js. For user feedback: after saving, the primary buttons is put in 'complete' state. The modal is closed after 0,2 seconds. The cancel button can now be left out, and the file modal.mustache has been moved.

--- lib/mojura/webapp/mojura/views/pageview.rb
+++ lib/mojura/webapp/mojura/views/pageview.rb
@@ -209,7 +209,7 @@ module MojuraWebApp
 
 		def render
 			# preloading so all views can still affect the page object (i.e. to include css, js, etc.)
-			self.include_template_file('modal_template', 'webapp/mojura/views/modal.mustache')
+			self.include_template_file('modal_template', 'webapp/mojura/modals/modal.mustache')
 			if (WebApp.current_user.logged_in?)
 				type = Settings.get_b(:advanced_rights_control) ? 'advanced' : 'simple'
 				self.include_template_file('template-rights-controls', "webapp/mojura/modals/rights_controls_#{type}.mustache")
