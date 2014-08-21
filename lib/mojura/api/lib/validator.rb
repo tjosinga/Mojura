5ad258fbaf670b1fb8d9de64be27be6290b8ca9c
Taco Jan Osinga <info@osingasoftware.nl>
2014-08-21 18:50:12 +0200

    Adds date, time and datetime validation to the backend.

--- lib/mojura/api/lib/validator.rb
+++ lib/mojura/api/lib/validator.rb
@@ -42,6 +42,33 @@ module MojuraAPI
 			return self.matches_regexp?(value, /^(http|https):\/\/([a-zA-Z0-9\.\-]+(:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9\.,\?'\\\+&amp;%\$#=~_\-]+))*$/)
 		end
 
+		def self.is_date?(value)
+			return false if !value.is_a?(String) || value.empty?
+			begin
+				# Ensure it only includes date information, without time. This does check days in month, but not 29 Feb in non-leap years.
+				return false if !self.matches_regexp?(value, /^\d{4}-(((0?1|0?3|0?5|0?7|0?8|10|12)-(0?[1-9]|[12]?\d|3[01]))|((0?4|0?6|0?9|11)-(0?[1-9]|[12]?\d|30))|((0?2)-(0?[1-9]|[12]\d)))$/)
+				# Ensure it's a valid date
+				Date.parse(value)
+				return true
+			rescue ArgumentError
+				return false
+			end
+		end
+
+		def self.is_time?(value)
+			return self.matches_regexp?(value, /^([01]?[0-9]|2[0-3]):[0-5][0-9](:([0-5][0-9]))?$/)
+		end
+
+		def self.is_datetime?(value)
+			return false if !value.is_a?(String) || value.empty?
+			begin
+				Date.parse(value)
+				return true
+			rescue ArgumentError
+				return false
+			end
+		end
+
 		def self.in_array?(value, arr)
 			return (arr.to_a || []).include?(value)
 		end
--- test/core_validator.rb
+++ test/core_validator.rb
@@ -1,96 +1,122 @@
 $:.unshift File.join(File.dirname(__FILE__), '..')
 require 'test/unit'
-require 'api/lib/validator'
+require 'lib/mojura/api/lib/validator'
 
 module MojuraAPI
 
 	class ValidatorTester < Test::Unit::TestCase
 
 		def test_matches_regexp
-			assert(Validator.matches_regexp('This is a test of the JavaScript RegExp object', /\bt[a-z]+\b/))
-			assert(!Validator.matches_regexp('This is a test of the JavaScript RegExp object', /\bt[0-9]+\b/))
+			assert(Validator.matches_regexp?('This is a test of the JavaScript RegExp object', /\bt[a-z]+\b/))
+			assert(!Validator.matches_regexp?('This is a test of the JavaScript RegExp object', /\bt[0-9]+\b/))
 		end
 
 		def test_is_required
-			assert(Validator.is_required('test'))
-			assert(Validator.is_required(0))
-			assert(Validator.is_required(-1))
-			assert(Validator.is_required(1))
-			assert(Validator.is_required(0.000001))
-			assert(Validator.is_required(1))
-			assert(!Validator.is_required(nil))
+			assert(Validator.is_required?('test'))
+			assert(Validator.is_required?(0))
+			assert(Validator.is_required?(-1))
+			assert(Validator.is_required?(1))
+			assert(Validator.is_required?(0.000001))
+			assert(Validator.is_required?(1))
+			assert(!Validator.is_required?(nil))
 		end
 
 		def test_is_numeric
-			assert(Validator.is_numeric('0'))
-			assert(Validator.is_numeric('-12'))
-			assert(Validator.is_numeric(12))
-			assert(!Validator.is_numeric(1.2))
-			assert(!Validator.is_numeric('1.2'))
-			assert(!Validator.is_numeric('true'))
-			assert(!Validator.is_numeric(true))
-			assert(!Validator.is_numeric(nil))
-			assert(!Validator.is_numeric({}))
-			assert(!Validator.is_numeric([]))
+			assert(Validator.is_numeric?('0'))
+			assert(Validator.is_numeric?('-12'))
+			assert(Validator.is_numeric?(12))
+			assert(!Validator.is_numeric?(1.2))
+			assert(!Validator.is_numeric?('1.2'))
+			assert(!Validator.is_numeric?('true'))
+			assert(!Validator.is_numeric?(true))
+			assert(!Validator.is_numeric?(nil))
+			assert(!Validator.is_numeric?({}))
+			assert(!Validator.is_numeric?([]))
 		end
 
 		def test_is_email
 			# Source for (in)valid adresses: http://en.wikipedia.org/wiki/Email_address
 			# Regular expression of mail doesn't get follow all rules, which are commented. Acceptable marge
-			assert(Validator.is_email('niceandsimple@example.com'))
-			assert(Validator.is_email('simplewith+symbol@example.com'))
-			assert(Validator.is_email('less.common@example.com'))
-			assert(Validator.is_email('a.little.more.unusual@dept.example.com'))
-			# assert(Validator.is_email("'@[10.10.10.10]"))
-			# assert(Validator.is_email("user@[IPv6:2001:db8:1ff::a0b:dbd0]"))
-			# assert(Validator.is_email("\"much.more\ unusual\"@example.com"))
-			# assert(Validator.is_email("\"very.unusual.@.unusual.com\"@example.com"))
-			# assert(Validator.is_email("\"very.(),:;<>[]\\\".VERY.\\\"very@\\\\\\ \\\"very\\\".unusual\"@strange.example.com"))
-			# assert(Validator.is_email("0@a"))
-			# assert(Validator.is_email("!#\$%&'*+-/=?^_`{}|~@example.org"))
-			# assert(Validator.is_email("\"()<>[]:,;@\\\"!#\$%&'*+-/=?^_`{}|\ \ ~\ \ \ ?\ \ \ ^_`{}|~.a\"@example.org"))
-			# assert(Validator.is_email("\"\"@example.org"))
-			# assert(Validator.is_email("postbox@com"))
+			assert(Validator.is_email?('niceandsimple@example.com'))
+			assert(Validator.is_email?('simplewith+symbol@example.com'))
+			assert(Validator.is_email?('less.common@example.com'))
+			assert(Validator.is_email?('a.little.more.unusual@dept.example.com'))
+			# assert(Validator.is_email?("'@[10.10.10.10]"))
+			# assert(Validator.is_email?("user@[IPv6:2001:db8:1ff::a0b:dbd0]"))
+			# assert(Validator.is_email?("\"much.more\ unusual\"@example.com"))
+			# assert(Validator.is_email?("\"very.unusual.@.unusual.com\"@example.com"))
+			# assert(Validator.is_email?("\"very.(),:;<>[]\\\".VERY.\\\"very@\\\\\\ \\\"very\\\".unusual\"@strange.example.com"))
+			# assert(Validator.is_email?("0@a"))
+			# assert(Validator.is_email?("!#\$%&'*+-/=?^_`{}|~@example.org"))
+			# assert(Validator.is_email?("\"()<>[]:,;@\\\"!#\$%&'*+-/=?^_`{}|\ \ ~\ \ \ ?\ \ \ ^_`{}|~.a\"@example.org"))
+			# assert(Validator.is_email?("\"\"@example.org"))
+			# assert(Validator.is_email?("postbox@com"))
 
-			assert(!Validator.is_email('Abc.example.com'))
-			# assert(!Validator.is_email("Abc.@example.com"))
-			# assert(!Validator.is_email("Abc..123@example.com"))
-			assert(!Validator.is_email('A@b@c@example.com'))
-			assert(!Validator.is_email("a\"b(c)d,e:f;g<h>i[j\k]l@example.com"))
-			assert(!Validator.is_email("just\"not\"right@example.com"))
-			assert(!Validator.is_email("this is\"not\allowed@example.com"))
-			assert(!Validator.is_email("\"this\\ still\\\"not\\\\allowed@example.com"))
+			assert(!Validator.is_email?('Abc.example.com'))
+			# assert(!Validator.is_email?("Abc.@example.com"))
+			# assert(!Validator.is_email?("Abc..123@example.com"))
+			assert(!Validator.is_email?('A@b@c@example.com'))
+			assert(!Validator.is_email?("a\"b(c)d,e:f;g<h>i[j\k]l@example.com"))
+			assert(!Validator.is_email?("just\"not\"right@example.com"))
+			assert(!Validator.is_email?("this is\"not\allowed@example.com"))
+			assert(!Validator.is_email?("\"this\\ still\\\"not\\\\allowed@example.com"))
 
 		end
 
 		def test_is_website
-			assert(Validator.is_website_url('http://www.osingasoftware.nl'))
-			assert(Validator.is_website_url('http://www.cmd.tech.nhl.nl'))
-			assert(Validator.is_website_url('http://www.cmd.tech.nhl.nl/?test=sadlifjamwlrjasvlusmvietia&amp;afdsasdfasl.kjfasfasdf'))
-			assert(!Validator.is_website_url('www.osingasoftware.nl'))
-			assert(!Validator.is_website_url('taco@osisoft.nl'))
-			assert(!Validator.is_website_url(123))
-			assert(!Validator.is_website_url(true))
-			assert(!Validator.is_website_url(nil))
-			assert(!Validator.is_website_url({}))
-			assert(!Validator.is_website_url([]))
+			assert(Validator.is_url?('http://www.osingasoftware.nl'))
+			assert(Validator.is_url?('http://www.cmd.tech.nhl.nl'))
+			assert(Validator.is_url?('http://www.cmd.tech.nhl.nl/?test=sadlifjamwlrjasvlusmvietia&amp;afdsasdfasl.kjfasfasdf'))
+			assert(!Validator.is_url?('www.osingasoftware.nl'))
+			assert(!Validator.is_url?('taco@osisoft.nl'))
+			assert(!Validator.is_url?(123))
+			assert(!Validator.is_url?(true))
+			assert(!Validator.is_url?(nil))
+			assert(!Validator.is_url?({}))
+			assert(!Validator.is_url?([]))
 		end
 
 		def test_in_array
-			assert(Validator.in_array('test', ['test', 'test 2']))
-			assert(!Validator.in_array('test', ['test 1', 'test 2']))
-			assert(Validator.in_array(5, [1, 1, 2, 3, 5, 8]))
-			assert(!Validator.in_array(6, [1, 1, 2, 3, 5, 8]))
-			assert(!Validator.in_array('test', []))
-			assert(!Validator.in_array('test', nil))
-			assert(!Validator.in_array('test', {test: 'test'}))
-			assert(!Validator.in_array('test', {test: 'test 1'}))
-			assert(!Validator.in_array([2, 5], [1, 1, 2, 3, 5, 8]))
-			assert(Validator.in_array([2, 5], [[1, 1], [2, 5]]))
-			assert(!Validator.in_array([6, 5], [1, 1, 2, 3, 5, 8]))
-			assert(!Validator.in_array([6, 5], nil))
+			assert(Validator.in_array?('test', ['test', 'test 2']))
+			assert(!Validator.in_array?('test', ['test 1', 'test 2']))
+			assert(Validator.in_array?(5, [1, 1, 2, 3, 5, 8]))
+			assert(!Validator.in_array?(6, [1, 1, 2, 3, 5, 8]))
+			assert(!Validator.in_array?('test', []))
+			assert(!Validator.in_array?('test', nil))
+			assert(!Validator.in_array?('test', {test: 'test'}))
+			assert(!Validator.in_array?('test', {test: 'test 1'}))
+			assert(!Validator.in_array?([2, 5], [1, 1, 2, 3, 5, 8]))
+			assert(Validator.in_array?([2, 5], [[1, 1], [2, 5]]))
+			assert(!Validator.in_array?([6, 5], [1, 1, 2, 3, 5, 8]))
+			assert(!Validator.in_array?([6, 5], nil))
 		end
 
+		def test_is_time
+			assert(Validator.is_time?('00:00'));
+			assert(Validator.is_time?('12:12'));
+			assert(Validator.is_time?('23:00'));
+			assert(Validator.is_time?('23:00'));
+			assert(Validator.is_time?('23:59'));
+			assert(Validator.is_time?('23:59:59'));
+			assert(!Validator.is_time?('24:00'));
+			assert(!Validator.is_time?('23:59:60'));
+			assert(!Validator.is_time?('0:0'));
+			assert(!Validator.is_time?('0:60'));
+			assert(!Validator.is_time?('0:600'));
+			assert(!Validator.is_time?('35:600'));
+			assert(!Validator.is_time?('12:0:00'));
+			assert(!Validator.is_time?('12:10:0'));
+		end
+
+		def test_is_date
+			assert(Validator.is_date?('2014-01-01'));
+			assert(Validator.is_date?('0000-01-01'));
+			assert(Validator.is_date?('9999-12-31'));
+			assert(Validator.is_date?('2014-9-12'));
+			assert(Validator.is_date?('2014-9-30'));
+			assert(!Validator.is_date?('9999-13-31'));
+			assert(!Validator.is_date?('2014-9-31'));
+		end
 	end
 
 end
