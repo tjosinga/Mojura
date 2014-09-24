07106d36552462280ed3602653d86405af8f721b
Taco Jan Osinga <info@osingasoftware.nl>
2014-09-24 12:03:56 +0200

    First draft for a lightbox.

--- lib/mojura/webapp/mojura/views/pageview.rb
+++ lib/mojura/webapp/mojura/views/pageview.rb
@@ -209,7 +209,8 @@ module MojuraWebApp
 
 		def render
 			# preloading so all views can still affect the page object (i.e. to include css, js, etc.)
-			self.include_template_file('modal_template', 'webapp/mojura/modals/modal.mustache')
+			self.include_template_file('template-modal', 'webapp/mojura/modals/modal.mustache')
+			self.include_template_file('template-lightbox', 'webapp/mojura/modals/lightbox.mustache')
 			if (WebApp.current_user.logged_in?)
 				type = Settings.get_b(:advanced_rights_control) ? 'advanced' : 'simple'
 				self.include_template_file('template-rights-controls', "webapp/mojura/modals/rights_controls_#{type}.mustache")
