e1537e65e8b09d291b2fe657f5d4f0f33baf5dda
Taco Jan Osinga <info@osingasoftware.nl>
2014-09-25 11:09:41 +0200

    Fixes another minor bug in OrderedHash.to_json.

--- lib/mojura/api/lib/datatypes.rb
+++ lib/mojura/api/lib/datatypes.rb
@@ -28,7 +28,7 @@ class BSON::OrderedHash
 		inject({}) { |acc, element| k,v = element; acc[k] = (if v.class == BSON::OrderedHash then v.to_h else v end); acc }
 	end
 
-	def to_json(options)
+	def to_json(options = {})
 		JSON.generate(to_h, options) rescue '{}'
 	end
 
