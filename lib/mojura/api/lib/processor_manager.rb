1497653b3d5cf8dcfff82b879ab2af0bfc517ef6
Taco Jan Osinga <info@osingasoftware.nl>
2015-03-02 14:59:42 +0100

    Update ProcessorManager: adds support for output filters.

--- lib/mojura/api/lib/processor_manager.rb
+++ lib/mojura/api/lib/processor_manager.rb
@@ -1,3 +1,5 @@
+require 'logger'
+
 module MojuraAPI
 
 	# The ProcessorManager manages pre- and postprocessors. The processors can subscribe to an api resource and method,
@@ -30,10 +32,13 @@ module MojuraAPI
 				end
 			}
 			class_name = processor.class.name
-			if subscribed_types.empty?
-				API.log.warn("Processor #{class_name} should at least contain method run_preprocessor or run_postprocessor.")
-			else
-				API.log.info("Subscribe processor #{class_name} to '#{resource}' as " + subscribed_types.join(' and '))
+			has_api = API.log.respond_to?(:warn) rescue false
+			if has_api
+				if subscribed_types.empty?
+					API.log.warn("Processor #{class_name} should at least contain method run_preprocessor or run_postprocessor.")
+				else
+					API.log.info("Subscribe processor #{class_name} to '#{resource}' as " + subscribed_types.join(' and '))
+				end
 			end
 		end
 
@@ -55,7 +60,7 @@ module MojuraAPI
 
 		# Runs the postprocessors which should be run, based on the resource and the method.
 		def run_postprocessors(resource, method, input, output)
-			processors = get_processors(:postprocessor, resource, method, input)
+			processors = get_processors(:postprocessor, resource, method, input, output)
 			processors.each { | processor |
 				options = { resource: resource, method: method, processor_options: processor[:options] }
 				if (!processor[:options][:async].is_a?(FalseClass))
@@ -70,7 +75,7 @@ module MojuraAPI
 		end
 
 		# Returns all processors which should be run, based on the resource and the method.
-		def get_processors(type, resource, method, input)
+		def get_processors(type, resource, method, input, output = {})
 			return [] unless @subscriptions.include?(type) &&
 			                 @subscriptions[type].include?(method)
 			result = []
@@ -78,7 +83,10 @@ module MojuraAPI
 				if resource.match(/^#{subscribed_resource}$/)
 					processors.each { | processor |
 						input_filter = processor[:options][:input_filter] || {}
-						result.push(processor) if (input_filter.size == 0) || (input_filter.all? { | k, v | input[k] == v })
+						output_filter = processor[:options][:output_filter] || {}
+						input_filter_check = ((input_filter.size == 0) || (input_filter.all? { | k, v | input[k] == v }))
+						output_filter_check = ((output_filter.size == 0) || (output_filter.all? { | k, v | output[k] == v }))
+						result.push(processor) if input_filter_check && output_filter_check
 					}
 				end
 			}
--- test/core_processors.rb
+++ test/core_processors.rb
@@ -30,37 +30,23 @@ module MojuraAPI
 
 	class ProcessorManagerTester < Test::Unit::TestCase
 
-		def test_bad_processor
-			assert_raise(RuntimeError) {
-				ProcessorManager.subscribe_preprocessor('salt', :get, PostProcessorMocker.new)
-			}
-
-			assert_raise(RuntimeError) {
-				ProcessorManager.subscribe_preprocessor('salt', :get, PostProcessorMocker.new)
-			}
-
-			assert_nothing_raised {
-				ProcessorManager.subscribe_preprocessor('salt', :get, PreProcessorMocker.new)
-			}
-
-			assert_nothing_raised {
-				ProcessorManager.subscribe_postprocessor('salt', :get, PostProcessorMocker.new)
-			}
-		end
-
 		def test_run_postprocessors
 			p1 = PostProcessorMocker.new
 			p2 = PostProcessorMocker.new
 
-			ProcessorManager.subscribe_postprocessor('resource1', :get, p1)
-			ProcessorManager.subscribe_postprocessor('resource2', :get, p1)
-			ProcessorManager.subscribe_postprocessor('resource3', :get, p1, input_filter: {test1: 'Hello'})
+			ProcessorManager.subscribe_processor('resource1', :get, p1)
+			ProcessorManager.subscribe_processor('resource2', :get, p1)
+			ProcessorManager.subscribe_processor('resource3', :get, p1, input_filter: {test1: 'Hello'})
 
-			ProcessorManager.subscribe_postprocessor('resource2', :get, p2)
-			ProcessorManager.subscribe_postprocessor('resource3', :get, p2, input_filter: {test2: 'Hello'})
+			ProcessorManager.subscribe_processor('resource2', :get, p2)
+			ProcessorManager.subscribe_processor('resource3', :get, p2, input_filter: {test2: 'Hello'})
 
-			ProcessorManager.subscribe_postprocessor('resource4/\w+', :get, p2)
-			ProcessorManager.subscribe_postprocessor('resource5/\w+/subresource', :get, p2)
+			ProcessorManager.subscribe_processor('resource4/\w+', :get, p2)
+			ProcessorManager.subscribe_processor('resource5/\w+/subresource', :get, p2)
+
+			ProcessorManager.subscribe_processor('resource6', :get, p2, output_filter: {test7: 10})
+			ProcessorManager.subscribe_processor('resource7', :get, p2, output_filter: {test7: 12})
+
 
 			input1 = { test1: 'Hello', test2: 'World' }
 			input2 = { test5: 'Hello' }
@@ -71,11 +57,13 @@ module MojuraAPI
 
 			ProcessorManager.run_postprocessors('resource1', :get, input1, output1)
 			ProcessorManager.run_postprocessors('resource1', :post, input2, output2)
+			sleep(0.05) # Wait for async to finish
 			assert_equal(input1, p1.last_input);
 			assert_equal(output1, p1.last_output);
 			assert_equal({resource: 'resource1', method: :get, processor_options: {}}, p1.last_options)
 
 			ProcessorManager.run_postprocessors('resource2', :get, input2, output2)
+			sleep(0.05) # Wait for async to finish
 			assert_equal(input2, p1.last_input);
 			assert_equal(output2, p1.last_output);
 			assert_equal({resource: 'resource2', method: :get, processor_options: {}}, p1.last_options)
@@ -85,20 +73,32 @@ module MojuraAPI
 			assert_equal({resource: 'resource2', method: :get, processor_options: {}}, p2.last_options)
 
 			ProcessorManager.run_postprocessors('resource3', :get, input1, output3)
+			sleep(0.05) # Wait for async to finish
 			assert_equal(output3, p1.last_output);
 			assert_not_equal(output3, p2.last_output);
 
 			ProcessorManager.run_postprocessors('resource3', :get, input1, output3)
+			sleep(0.05) # Wait for async to finish
 			assert_equal(output3, p1.last_output);
 			assert_not_equal(output3, p2.last_output);
 
 			ProcessorManager.run_postprocessors('resource4/variableid', :get, input1, output1)
+			sleep(0.05) # Wait for async to finish
 			assert_not_equal(output1, p1.last_output);
 			assert_equal(output1, p2.last_output);
 
 			ProcessorManager.run_postprocessors('resource5/variable_id/subresource', :get, input1, output2)
+			sleep(0.05) # Wait for async to finish
 			assert_not_equal(output2, p1.last_output);
 			assert_equal(output2, p2.last_output);
+
+			ProcessorManager.run_postprocessors('resource6', :get, {}, output3)
+			sleep(0.05) # Wait for async to finish
+			assert_not_equal(output3, p2.last_output);
+
+			ProcessorManager.run_postprocessors('resource7', :get, {}, output3)
+			sleep(0.05) # Wait for async to finish
+			assert_equal(output3, p2.last_output);
 
 		end
 
