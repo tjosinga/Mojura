d34f9ef5c7fca1ba7d872e856ebcac8082fbb1f4
Taco Jan Osinga <info@osingasoftware.nl>
2015-09-29 11:28:58 +0200

    Fixes syntax error.

--- lib/mojura/webapp/views/news/view_article.rb
+++ lib/mojura/webapp/views/news/view_article.rb
@@ -14,7 +14,7 @@ module MojuraWebApp
 			else
 				data = WebApp.api_call("news/#{@newsid}")
 				data[:show_title] = !Settings.get_b(:news, :title_as_page_title, false)
-				short_description = data[:content][:html]).split('. ').slice(0, 5).join('. ') #Sanitize.clean(data[:content][:html]).split('. ').slice(0, 5).join('. ') + '...' #contain 5 sentences
+				short_description = data[:content][:html].split('. ').slice(0, 5).join('. ') #Sanitize.clean(data[:content][:html]).split('. ').slice(0, 5).join('. ') + '...' #contain 5 sentences
 				short_description = short_description.split(' ').slice(0, 70).join(' ') if (short_description.split(' ').size > 80)
 
 				WebApp.page.data[:title] = data[:title].to_s unless data[:show_title]
